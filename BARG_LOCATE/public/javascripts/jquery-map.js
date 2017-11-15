416298d3c8353f577e33f3631e79ec294b73994d
Lý Thành Nhân <mrfrenkyztiny@gmail.com>
2017-11-15 17:45:20 +0700

    - fint shorted direction

--- BARG_LOCATE/public/javascripts/jquery-map.js
+++ BARG_LOCATE/public/javascripts/jquery-map.js
@@ -1,4 +1,3 @@
-let promise =require("bluebird")
 var map;
 var geocoder;
 var marker;
@@ -6,6 +5,7 @@ var motobike;
 let drivers
 let min_driver
 let circle
+
 var motobike_arr = []
 var default_position = {
     lat: 10.7666851,
@@ -26,18 +26,12 @@ function initMap() {
         center: default_position,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-    fetchDrivers(null, function (err, drivers) {
-        !!err ?
-            console.log(err) :
-            generateMotoBikeLocation(drivers, map)
-    })
     geocoder = new google.maps.Geocoder();
     document.getElementById('submit').addEventListener('click', function () {
         clearOverlays();
         geocodeAddress(geocoder, map);
     });
 }
-
 function filterDriversWithRadius(drivers, radius, center,directionsService) {
     return new Promise(function(resolve){
         const motobikes = []
@@ -45,7 +39,7 @@ function filterDriversWithRadius(drivers, radius, center,directionsService) {
         if (radius === "all") {
             resolve(drivers)
         } else {
-            Promise.map(drivers,(item)=>{
+            new Promise.map(drivers,function(item){
                 const drive = new google.maps.LatLng({
                     lat: item.lat,
                     lng: item.lng
@@ -53,14 +47,16 @@ function filterDriversWithRadius(drivers, radius, center,directionsService) {
                 let distance = google.maps.geometry.spherical.computeDistanceBetween(drive, center).toFixed(2);
                 if (distance <= parseInt(radius)) {
                     return getDirectionDistance(center,drive,directionsService).then(value=>{
-                        return item.value=value
+                        item.value = value
+                        return item
                     })
                 }
             })
-            .then(array_results=>{
-                console.log("array_result", array_results)
+            .then(function(array_results){
+                array_results = array_results.filter(item=>!!item)
+                resolve(array_results)
             })
-            }
+        }
     })
 }
 
@@ -75,8 +71,7 @@ function getDirectionDistance(center, driver, directionsService) {
     };
     directionsService.route(request, function (result, status) {
         if (status == 'OK') {
-            console.log("result_38", result.routes[0].legs[0].distance.value)
-            callback(result.routes[0].legs[0].distance.value)
+            resolve(result.routes[0].legs[0].distance.value)
         }
     })
 })  
@@ -99,18 +94,38 @@ function geocodePosition(pos) {
         infowindow.open(map, marker);
     });
 }
+function DrawDirection(start,end){
+    directionsDisplay = new google.maps.DirectionsRenderer;
+    let min = drivers[0].value.routes[0].legs[0].distance.value
+    let index_min=null
+    let next = null
+    for (let i = 1;i < drivers.length-1; i++){
+        next = drivers[i].value.routes[0].legs[0].distance.value
+        if( min > next){
+            index_min = i
+            min = next 
+        }
+    }
+    return directionsDisplay.setDirections(drivers[index_min].value);
 
-function generateMotoBikeLocation(drivers, map) {
+}
+function generateMotoBikeLocation(drivers,center,map) {
     icon = {
         url: 'https://image.flaticon.com/icons/svg/296/296210.svg',
         scaledSize: new google.maps.Size(50, 50),
         origin: new google.maps.Point(0, 0),
         anchor: new google.maps.Point(30, 30)
     };
+    let min = drivers[0].value
+    let min_pos=null
     for (let i = 0; i < drivers.length; i++) {
         let pos = {
             lat: drivers[i].lat,
             lng: drivers[i].lng
+        }
+        if(min>drivers[i].value){
+            min=drivers[i].value
+            min_pos = pos
         }
         motobike = new google.maps.Marker({
             map: map,
@@ -119,6 +134,19 @@ function generateMotoBikeLocation(drivers, map) {
             draggable: true,
         })
         motobike_arr.push(motobike)
+    }
+    if(center && min_pos){
+        var request = {
+            origin: new google.maps.LatLng(min_pos),
+            destination:  center,
+            travelMode: 'DRIVING'
+          };
+        marker.setMap(null)
+        directionsService.route(request,function(result,status){
+            if (status == 'OK') {
+                directionsDisplay.setDirections(result);
+              }
+        })
     }
 }
 
@@ -169,7 +197,8 @@ function geocodeAddress(geocoder, resultsMap) {
                     console.log(err) :
                         filterDriversWithRadius(drivers, radius, results[0].geometry.location,directionsService)
                     .then(result=>{
-                        generateMotoBikeLocation(result, resultsMap)
+                        directionsDisplay.setMap(resultsMap)  
+                        generateMotoBikeLocation(result,results[0].geometry.location, resultsMap)
                     })
             })
             google.maps.event.addListener(marker, 'dragend', function () {
