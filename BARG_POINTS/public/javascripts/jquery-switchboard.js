396839fbb3be723702c348ddd941127a7b85880f
Thái Nguyễn Hoàng Phát <thainguyenhoangphat1996@gmail.com>
2017-11-16 16:39:59 +0700

    abc

--- BARG_POINTS/public/javascripts/jquery-switchboard.js
+++ BARG_POINTS/public/javascripts/jquery-switchboard.js
@@ -1,3 +1,4 @@
+let val = null;
 $(document).ready(function () {
     let socket = io('http://localhost:8000');
     let socket_id = null;
@@ -25,16 +26,27 @@ $(document).ready(function () {
         }
         return '<tr><td>'+point.id+'</td><td>'+point.address+'</td><td>'+point.type+'</td><td>'+point.note+'</td><td>'+point.status+'</td></tr>';
     }
-
+    function toastMsg(msg) {
+        $("#animate" ).html(msg);
+        $('#animate').show();
+        $("#animate" ).animate({
+            opacity: 0.25,
+        }, 2000, function() {
+            // Animation complete.
+            $("#animate" ).css('opacity', 1);
+            $('#animate').hide();
+        });
+    }
     $('#sel').on('change', function(){
 
-        let val = $(this).val();
+        val = $(this).val();
 
         switch(val){
             case "0":
                 socket.emit('get-point-not-located', socket_id);
                 socket.on('get-point-not-located', function (ls_point) {
-                    alert('Có thêm điểm chưa định vị');
+                    alert('case ' + val);
+                    toastMsg('Có thêm điểm chưa định vị');
                     let tr = '';
                     for(i=0; i< ls_point.length; i++) {
 
@@ -46,7 +58,8 @@ $(document).ready(function () {
             case "1":
                 socket.emit('get-point-is-locating', socket_id);
                 socket.on('get-point-is-locating', function (ls_point) {
-                    alert('Có thêm điểm đang định vị');
+                    alert('case ' + val);
+                    toastMsg('Có thêm điểm đang định vị');
                     let tr = '';
                     for(i=0; i< ls_point.length; i++) {
 
